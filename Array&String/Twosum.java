f2a9ccf99a319f38567bfeab3c83982f1aca04f4
Mohsin Khan <mohsinkhan85090@gmail.com>
2025-08-12 20:16:08 +0530

    Add Twosum.java

--- Array&String/Twosum.java
+++ Array&String/Twosum.java
@@ -14,3 +14,21 @@ class Solution {
         return new int[]{-1,-1};
     }
 }
+// optimal approach
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> mp = new HashMap<>();
+        int n= nums.length;
+        for(int i=0;i<n;i++)
+        {
+            int num =nums[i];
+            int more= target - num;
+            if(mp.containsKey(more))
+            {
+                return new int[]{mp.get(more),i};
+            }
+            mp.put(num, i);
+        }
+        return new int []{};
+    }
+}
