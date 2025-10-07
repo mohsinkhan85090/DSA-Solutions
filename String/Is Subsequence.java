fc703f752810cb05c323225d74664b4235b0f552
Mohsin Khan <mohsinkhan800213@gmail.com>
2025-10-07 20:11:08 +0530

    Is Subsequence

---
+++ String/Is Subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+     int a=0;
+     int b=0;
+     while(a<s.length() && b<t.length())
+     {
+        if(a<s.length()&& s.charAt(a) == t.charAt(b))
+        {
+            a++;
+        }
+        b++;
+     } 
+     return a==s.length();
+    }
+}
