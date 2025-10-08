2581923bf5602fe1475453a5e3a793995a53f213
Mohsin Khan <mohsinkhan800213@gmail.com>
2025-10-08 21:50:58 +0530

    Two Sum

---
+++ Array/Two Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int start =0;
+        int end =nums.length -1;
+        while(start <end )
+        {
+            int sum = nums[start ] +nums[end];
+            if(sum == target )
+            {
+                return new int []{start,end};
+            }
+            else if(sum >target)
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+        return new int[]{};
+    }
+}
