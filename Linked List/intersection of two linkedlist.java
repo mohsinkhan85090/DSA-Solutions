577aed61f07c6f993c19d038fdad724f7bcd7f8d
Mohsin Khan <mohsinkhan800213@gmail.com>
2025-10-14 09:42:59 +0530

    changes

---
+++ Linked List/intersection of two linkedlist.java
@@ -0,0 +1,26 @@
+ublic class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA,B= headB;
+        while(A!=B)
+        {
+            if(A==null)
+            {
+                A= headB;
+            }
+            else
+            {
+                A= A.next;
+            }
+            if(B==null)
+            {
+                B= headA;
+            }
+            else
+            {
+                B= B.next;
+            }
+        }
+        return A;
+        
+    }
+}
