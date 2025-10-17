6e3ef3f5c1cad43ea1edc62f7c7c0e6ac89bdd0b
Mohsin Khan <mohsinkhan800213@gmail.com>
2025-10-17 20:56:29 +0530

    changes

---
+++ Stack/Clear Digits.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> st= new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c= s.charAt(i);
+            if (Character.isDigit(c)) {
+                if (!st.isEmpty()) {
+                    st.pop(); 
+                }
+            } else {
+                st.push(c); 
+            }
+        
+    } 
+    String result = "";
+        for (int i = 0; i < st.size(); i++) {
+            result = result + st.get(i);
+        }
+
+        return result;
+}
+}
