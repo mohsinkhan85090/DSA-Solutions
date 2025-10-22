559e552fcc41577583d9b5dacc5c4bcc152c85cd
Mohsin Khan <mohsinkhan800213@gmail.com>
2025-10-22 21:50:43 +0530

    :

--- Stack & Queue/Time Needed to Buy Tickets.java
+++ Stack & Queue/Time Needed to Buy Tickets.java
@@ -16,3 +16,33 @@ class Solution {
         return time;
     }
 }
+
+
+/**
+ *class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < tickets.length; i++) {
+            q.add(i);
+        }
+
+        int time = 0;
+
+        while (!q.isEmpty()) {
+            int i = q.poll();
+            tickets[i]--;
+            time++;
+
+            if (tickets[i] > 0) {
+                q.add(i);
+            }
+
+            if (i == k && tickets[i] == 0) {
+                break;
+            }
+        }
+
+        return time;
+    }
+}
+ */
